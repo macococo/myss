ee34db97744523e42d37a255bae39797b8805036
Makoto Kudo <dev@mof-brown.jp>
2014-07-28 00:24:28 +0900

    remove mkdirp module

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="underscore/underscore.d.ts" />
 /// <reference path="promise/promise.d.ts" />
-/// <reference path="mkdirp/mkdirp.d.ts" />
 /// <reference path="commander/commander.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
 /// <reference path="fs-extra/fs-extra.d.ts" />
